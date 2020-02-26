aca31af47ef1b7728573e7085d8c62c62eb87405
DustinKLo <dustinlo.pcc@gmail.com>
2020-02-25 21:20:16 -0800

    removed celery import app from __init__.py

--- hysds/__init__.py
+++ hysds/__init__.py
@@ -3,8 +3,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-from hysds.celery import app
-
 __version__ = "0.4.0"
 __url__ = "https://github.com/hysds/hysds"
 __description__ = "HySDS (Hybrid Cloud Science Data System)"
