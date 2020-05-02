0c8b0524c089254485d73f1363fc0f4fd687bf0c
DustinKLo <dustinlo.pcc@gmail.com>
2020-05-02 11:12:03 -0700

    added AWS_REGION to celeryconfig and edited get_grq_es() to use AWS ES

--- hysds/es_util.py
+++ hysds/es_util.py
@@ -3,6 +3,10 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
+from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
+from elasticsearch import RequestsHttpConnection
+import urllib3
+
 from hysds.celery import app
 from hysds.log_utils import logger
 
@@ -10,6 +14,8 @@ try:
     from hysds_commons.elasticsearch_utils import ElasticsearchUtility
 except (ImportError, ModuleNotFoundError):
     logger.error('Cannot import hysds_commons.elasticsearch_utils')
+
+urllib3.disable_warnings()
 
 MOZART_ES = None
 GRQ_ES = None
@@ -24,6 +30,23 @@ def get_mozart_es():
 
 def get_grq_es():
     global GRQ_ES
+
     if GRQ_ES is None:
-        GRQ_ES = ElasticsearchUtility(app.conf.GRQ_ES_URL, logger)
+        aws_es = app.conf['GRQ_AWS_ES']
+        es_host = app.conf['GRQ_ES_HOST']
+        es_url = app.conf['GRQ_ES_URL']
+        region = app.conf['AWS_REGION']
+
+        if aws_es:
+            aws_auth = BotoAWSRequestsAuth(aws_host=es_host, aws_region=region, aws_service='es')
+            GRQ_ES = ElasticsearchUtility(
+                es_url=es_host,
+                logger=logger,
+                http_auth=aws_auth,
+                connection_class=RequestsHttpConnection,
+                use_ssl=True,
+                verify_certs=False,
+            )
+        else:
+            GRQ_ES = ElasticsearchUtility(es_url, logger)
     return GRQ_ES
