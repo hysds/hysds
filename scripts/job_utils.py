e48f78fc07dbb7c5edcb3bfcae61c03a962d11c7
Mohammed Karim <widewidesea2017@gmail.com>
2020-08-25 07:05:30 -0700

    update

---
+++ scripts/job_utils.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+from builtins import int
+from future import standard_library
+standard_library.install_aliases()
+import os
+import sys
+import json
+import time
+import traceback
+import logging
+import argparse
+import random
+import boto3
+import requests
+from datetime import datetime
+
+from hysds.utils import parse_iso8601
+from hysds.celery import app
+import hysds.es_util as es_util
+from elasticsearch import Elasticsearch, exceptions
+
+def get_timedout_query(timeout, status):
+    """Tag jobs stuck in job-started or job-offline that have timed out."""
+
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "terms": {
+                            "status": status
+                        }
+                    },
+                    {
+                        "range": {
+                            "@timestamp": {
+                                "lt": "now-%ds" % timeout
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        "_source": [
+            "status", "tags", "uuid", "celery_hostname",
+            "job.job_info.time_start", "job.job_info.time_limit"
+        ]
+    }
+    return query
+
+def run_query(query):
+    
+    print(query)
+    ES = es_util.get_mozart_es()
+    result = ES.search(index="job_status-current", body=json.dumps(query))
+    print("run_query : result : \n{}".format(json.dumps(result, indent = 2)))
+    return result
+
+
+def run_query_with_scroll(query, url="localhost:9200", index = "job_status-current"):
+    client = Elasticsearch(url)
+    resp = client.search(
+            index = index,
+            body = json.dumps(query),
+            scroll = '10s' # length of time to keep search context
+        )
+    old_scroll_id = resp['_scroll_id']
+    results = []
+    while len(resp['hits']['hits']):
+        resp = client.scroll(
+                scroll_id = old_scroll_id,
+                scroll = '10s' # length of time to keep search context
+            )
+        old_scroll_id = resp['_scroll_id']
+        if len(resp['hits']['hits']) == 0:
+            break
+        for hit in resp['hits']['hits']:
+            results.append(hit)
+    
+    return results
+
+def update_es(doc_id, data, url="localhost:9200", index = "job_status-current"):
+    client = Elasticsearch(url)
+    response = client.update(index=index, id=doc_id, body=data)
+    print(response)
+    return response
