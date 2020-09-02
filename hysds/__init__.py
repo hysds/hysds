00ace57c83ed37b085f3ebeff4e79e35240b09fe
Gerald Manipon <pymonger@gmail.com>
2020-09-02 15:59:32 -0700

    HC-266: remove syncer script bootstrap mechanism (#83)
    * remove bootstrap mechanism
    
    * bump version

--- hysds/__init__.py
+++ hysds/__init__.py
@@ -3,6 +3,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 __url__ = "https://github.com/hysds/hysds"
 __description__ = "HySDS (Hybrid Cloud Science Data System)"
