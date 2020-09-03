4e9be1b50de58c5e2306ae0310a7d96fa66b81d0
Gerald Manipon <pymonger@gmail.com>
2020-09-03 06:29:48 -0700

    restore ASG bootstrap but modify for immediate scale-up

--- scripts/sync_ec2_target_tracking_metric.py
+++ scripts/sync_ec2_target_tracking_metric.py
@@ -54,6 +54,14 @@ def get_desired_capacity(asg):
     return groups[0]["DesiredCapacity"]
 
 
+def bootstrap_asg(asg, desired):
+    """Bootstrap ASG's desired capacity."""
+
+    c = boto3.client("autoscaling")
+    r = c.set_desired_capacity(AutoScalingGroupName=asg, DesiredCapacity=desired)
+    return desired
+
+
 def submit_metric(queue, asg, metric, metric_ns):
     """Submit EC2 custom metric data."""
 
@@ -96,7 +104,13 @@ def daemon(queue, asg, interval, namespace, user="guest", password="guest"):
             logging.info("jobs_waiting for %s queue: %s" % (queue, job_count))
             desired_capacity = float(get_desired_capacity(asg))
             if desired_capacity == 0:
-                desired_capacity = 1.0
+                if job_count > 0:
+                    desired_capacity = float(bootstrap_asg(asg, job_count))
+                    logging.info(
+                        "bootstrapped ASG %s to desired=%s" % (asg, desired_capacity)
+                    )
+                else:
+                    desired_capacity = 1.0
             metric = job_count / desired_capacity
             submit_metric(queue, asg, metric, namespace)
         except Exception as e:
