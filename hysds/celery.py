8672de6194ff04c7141ddc24ec288fdf9349ada5
Lei Pan <leipan@jpl.nasa.gov>
2020-09-18 02:54:06 -0700

    get app.config from envvar now

--- hysds/celery.py
+++ hysds/celery.py
@@ -6,10 +6,16 @@ from __future__ import division
 from future import standard_library
 standard_library.install_aliases()
 from celery import Celery
+import os
 
+
+celery_cfg_module = os.environ.get('HYSDS_CELERY_CFG_MODULE', None)
+if celery_cfg_module is None:
+  print('env variable HYSDS_CELERY_CFG_MODULE must be set.')
 
 app = Celery('hysds')
-app.config_from_object('celeryconfig')
+app.config_from_envvar('HYSDS_CELERY_CFG_MODULE')
+### app.config_from_object('celeryconfig')
 
 
 if __name__ == '__main__':
