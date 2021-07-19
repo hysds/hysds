2c132e4f7a1db8357150a9d88b9c7c3d82a627cb
Gerald Manipon <pymonger@gmail.com>
2021-07-19 07:31:43 -0700

    bump version

--- hysds/__init__.py
+++ hysds/__init__.py
@@ -3,6 +3,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __url__ = "https://github.com/hysds/hysds"
 __description__ = "HySDS (Hybrid Cloud Science Data System)"
