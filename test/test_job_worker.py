5bc9fb45e542c091a58a947705a94f34948e3983
dustinlo <dustin.k.lo@jpl.nasa.gov>
2023-10-19 16:05:56 -0700

    no need for mock since we cleaned the es calls

--- test/test_job_worker.py
+++ test/test_job_worker.py
@@ -30,23 +30,22 @@ class TestJobWorkerFuncs(TestCase):
         shutil.rmtree(self.job_dir)
 
     def test_find_usage_stats(self):
-        with patch("elasticsearch.Elasticsearch", return_value=None), patch("opensearchpy.OpenSearch", return_value=None):
-            import hysds.job_worker
+        import hysds.job_worker
 
-            # copy example _docker_stats.json
-            stats_file = os.path.join(self.examples_dir, "_docker_stats.json")
-            shutil.copy(stats_file, self.job_dir)
-            subdir = os.path.join(self.job_dir, "subdir1", "subdir2")
-            os.makedirs(subdir)
-            shutil.copy(stats_file, subdir)
+        # copy example _docker_stats.json
+        stats_file = os.path.join(self.examples_dir, "_docker_stats.json")
+        shutil.copy(stats_file, self.job_dir)
+        subdir = os.path.join(self.job_dir, "subdir1", "subdir2")
+        os.makedirs(subdir)
+        shutil.copy(stats_file, subdir)
 
-            # expected results
-            expected_stats_file = os.path.join(self.job_dir, "_docker_stats.json")
-            expected_stats_file2 = os.path.join(subdir, "_docker_stats.json")
+        # expected results
+        expected_stats_file = os.path.join(self.job_dir, "_docker_stats.json")
+        expected_stats_file2 = os.path.join(subdir, "_docker_stats.json")
 
-            # test execution
-            result = hysds.job_worker.find_usage_stats(self.job_dir)
+        # test execution
+        result = hysds.job_worker.find_usage_stats(self.job_dir)
 
-            # assertions
-            self.assertTrue(expected_stats_file in result)
-            self.assertTrue(expected_stats_file2 in result)
+        # assertions
+        self.assertTrue(expected_stats_file in result)
+        self.assertTrue(expected_stats_file2 in result)
