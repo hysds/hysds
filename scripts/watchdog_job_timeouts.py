0f2a8a99ac587fecc8e806ed9b24073341ee0797
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-03-15 12:01:51 -0700

    sort by latest results

--- scripts/watchdog_job_timeouts.py
+++ scripts/watchdog_job_timeouts.py
@@ -69,7 +69,13 @@ def tag_timedout_jobs(url, timeout):
 
         if status == "job-started":
             # get task info
-            task_query = {"query": {"term": {"_id": task_id}}, "_source": ["status", "event"]}
+            # {"sort": [{product_received_time: {"order": "asc"}}]}
+            task_query = {
+                "query": {
+                    "term": {"_id": task_id}},
+                    "_source": ["status", "event"],
+                    "sort": [{"@timestamp": {"order": "desc"}}]
+            }
             task_res = job_utils.es_query(task_query, index="task_status-current")
 
             if len(task_res["hits"]["hits"]) == 0:
