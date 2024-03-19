54334f2329f04e96b4be1a7f8209c51c644348e6
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-03-19 15:38:43 -0700

    HC-518: Add mechanism to optionally allow partitioning of triage indices

--- hysds/triage.py
+++ hysds/triage.py
@@ -23,6 +23,7 @@ import hysds
 from hysds.utils import makedirs
 from hysds.log_utils import logger
 from hysds.dataset_ingest import publish_dataset
+from hysds.celery import app
 
 
 def triage(job, ctx):
@@ -89,6 +90,15 @@ def triage(job, ctx):
         "version": "v{}".format(hysds.__version__),
         "label": "triage for job {}".format(parsed_job_id),
     }
+    triage_partition_format = app.conf.get("TRIAGE_PARTITION_FORMAT", None)
+    if triage_partition_format:
+        index_met = {
+            "index": {
+                "suffix": f"{ds['version']}_{datetime.utcnow().strftime(triage_partition_format)}"
+            }
+        }
+        ds.update(index_met)
+    logger.info(f"dataset info:\n{json.dumps(ds, indent=2)}")
     if "cmd_start" in job["job_info"]:
         ds["starttime"] = job["job_info"]["cmd_start"]
     if "cmd_end" in job["job_info"]:
