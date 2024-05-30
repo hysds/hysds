56f4c66610a4fa42277ae7d3e0e2acac536f176e
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-05-30 15:22:08 -0700

    revert

--- setup.py
+++ setup.py
@@ -44,9 +44,7 @@ setup(
         "pytz",
         "pytest",
         "tabulate>=0.8.6",
-        "pyyaml",
-        # pin setuptools until this is fixed: https://github.com/pypa/setuptools/issues/4399
-        "setuptools<70.0.0"
+        "pyyaml"
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
