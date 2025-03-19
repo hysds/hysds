18105aebd1a8b1a663906514c3cba709deed20c1
Gerald Manipon <pymonger@gmail.com>
2025-03-19 00:09:54 +0000

    remove unused imports

--- scripts/sync_ec2_target_tracking_metric.py
+++ scripts/sync_ec2_target_tracking_metric.py
@@ -6,9 +6,6 @@ from __future__ import absolute_import
 from future import standard_library
 
 standard_library.install_aliases()
-import os
-import sys
-import json
 import time
 import traceback
 import logging
